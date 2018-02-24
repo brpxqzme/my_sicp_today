cd5c396157c9605c6e88b8ed5d81e10c679ecbcc
Alfie Parthum <brpxqzme+github@gmail.com>
2018-02-24 16:15:41 -0500

    Correct comment about begin
    It is not exactly a procedure, just looks like one here.

--- 2/23.scm
+++ 2/23.scm
@@ -8,5 +8,5 @@
 
 ;; For the exercise, it is implied that the procedure takes one argument.
 ;; The built in for-each can handle procedures that take more than one.
-;; The procedure begin has not been introduced, but the anonymous lambda
-;; suffices for sequential execution.
+;; The begin syntax form has not been introduced, but the double-parenthesized
+;; anonymous lambda with no arguments suffices for sequential execution.
